b71a2cc9bb6147c511038e0dd30dd25a5ddc3238
s-kiips <ranjitkarsudeen14@gmail.com>
2017-10-28 12:21:29 +0545

    override in main app

--- src/main/java/com/sudin/SpringBootThymeleafApplication.java
+++ src/main/java/com/sudin/SpringBootThymeleafApplication.java
@@ -2,9 +2,17 @@ package com.sudin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringBootThymeleafApplication {
+public class SpringBootThymeleafApplication extends SpringBootServletInitializer {
+
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(SpringBootThymeleafApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootThymeleafApplication.class, args);
